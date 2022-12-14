25560229cee2ea3a204c74b946d15789e24b5641
lexie70 <alexandracampos658@gmail.com>
2022-12-14 04:14:32 +0000

    icon and number jv

--- src/app.js
+++ src/app.js
@@ -5,8 +5,36 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const iconos = ["♦", "♥", "♠", "♣"];
+
+function randomIcon() {
+  let test1 = Math.floor(Math.random() * iconos.length);
+  return iconos[test1];
+}
+
+function randonNumber() {
+  const numeros = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let test2 = Math.floor(Math.random() * numeros.length);
+  return numeros[test2];
+}
+
+window.onload = () => {
+  document.querySelector(".addIcon").innerHTML = randomIcon();
+  document.querySelector(".addIcon2").innerHTML = randomIcon();
+  document.querySelector(".addNumber").innerHTML = randonNumber();
 };
-// const figuritas [""]
